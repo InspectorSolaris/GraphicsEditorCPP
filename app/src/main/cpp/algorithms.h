f29708073b6d9c023f1dccbc7ea1f71a2dd082f0
herman <german.dark00@gmail.com>
2019-05-01 05:09:35 +0700

    minor fixes

--- app/src/main/cpp/algorithms.h
+++ app/src/main/cpp/algorithms.h
@@ -7,54 +7,67 @@
 #ifndef GRAPHICSEDITORCPP_ALGORITHMS_H
 #define GRAPHICSEDITORCPP_ALGORITHMS_H
 
+#include <jni.h>
+#include <android/bitmap.h>
+
 // parameters: original pic, angle of rotation
 // return: turned on specified angle picture
 
+extern "C" JNIEXPORT void JNICALL
 void pictureTurning();
 
 // parameters: original pic, ...
 // return: color corrected picture
 
+extern "C" JNIEXPORT void JNICALL
 void pictureColorcorrection();
 
 // parameters: original pic, scale ratio
 // return: scaled on specified ratio picture
 
+extern "C" JNIEXPORT void JNICALL
 void pictureScaling();
 
 // parameters: original pic, ...
 // return: ...
 
+extern "C" JNIEXPORT void JNICALL
 void pictureSegmentation();
 
 // parameters: original pic, retouching parameters
 // return: retouched pic
 
+extern "C" JNIEXPORT void JNICALL
 void pictureRetouching();
 
 // parameters: original pic, retouching parameters
 // return: masked pic
 
+extern "C" JNIEXPORT void JNICALL
 void pictureUnsharpMasking();
 
 // parameters: original pic, bilinear algorithm parameters
 // return: processed pic
 
+extern "C" JNIEXPORT void JNICALL
 void pictureBilinearFiltration();
 
 // parameters: original pic, trilinear algorithm parameters
 // return: processed pic
 
+extern "C" JNIEXPORT void JNICALL
 void pictureTrilinearFiltration();
 
 // parameters: original pic (clear pic), positions of points
 // return: interpolated with splines broken line on original pic
 
+extern "C" JNIEXPORT void JNICALL
 void drawLines();
 
 // parameters: map, start position, finish position
 // return: shortest way by A-Star
 
+extern "C" JNIEXPORT void JNICALL
 void algotihmAStar();
 
 #endif //GRAPHICSEDITORCPP_ALGORITHMS_H
