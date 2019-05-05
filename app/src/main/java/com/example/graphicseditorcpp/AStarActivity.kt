bb4fa519cf9c503baa2f16990acccc2a6b539ed1
DanilVBuh <danilvbuh@gmail.com>
2019-05-05 18:34:01 +0700

    added popup menu for astar

--- app/src/main/java/com/example/graphicseditorcpp/AStarActivity.kt
+++ app/src/main/java/com/example/graphicseditorcpp/AStarActivity.kt
@@ -2,11 +2,36 @@ package com.example.graphicseditorcpp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_astar.*
 
 class AStarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_astar)
+
+        aStarSettingsButton.setOnClickListener {
+            val popupMenu = android.support.v7.widget.PopupMenu(this, it)
+            popupMenu.inflate(R.menu.a_star_popup_menu)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when(item.itemId){
+                    R.id.resizeItem -> {
+                        Toast.makeText(this, "You tried to change size", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.firstEmpiric -> {
+                        Toast.makeText(this, "You chose first empiric", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    R.id.secondEmpiric -> {
+                        Toast.makeText(this, "You chose second empiric", Toast.LENGTH_SHORT).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+            popupMenu.show()
+        }
     }
 }
