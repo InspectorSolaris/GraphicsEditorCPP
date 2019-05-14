a2cf710cf3da1e70430f0857f1af1515d64e0262
rozanovasya <rozanovasya@gmail.com>
2019-05-14 14:30:34 +0700

    renaming in ScalingActivity

--- app/src/main/java/com/example/graphicseditorcpp/ScalingActivity.kt
+++ app/src/main/java/com/example/graphicseditorcpp/ScalingActivity.kt
@@ -10,7 +10,7 @@ import kotlinx.android.synthetic.main.activity_scaling.*
 
 class ScalingActivity : AppCompatActivity() {
 
-    var imageForTurningPath: String? = null
+    var imageForScalingPath: String? = null
 
     override fun onCreate(
         savedInstanceState: Bundle?
@@ -18,12 +18,12 @@ class ScalingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scaling)
 
-        imageForTurningPath = intent.getStringExtra("image")
-        imageForTurning.setImageURI(Uri.parse(imageForTurningPath))
+        imageForScalingPath = intent.getStringExtra("image")
+        imageForTurning.setImageURI(Uri.parse(imageForScalingPath))
     }
 
     fun scaling(n : Int) {
-        val imageBitmap : Bitmap = getBitmap(this.contentResolver, Uri.parse(imageForTurningPath))
+        val imageBitmap : Bitmap = getBitmap(this.contentResolver, Uri.parse(imageForScalingPath))
         val width = imageBitmap.width
         val height = imageBitmap.height
         val startPixels = IntArray(width*height)
